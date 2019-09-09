a0af4ab288a790942bf54afac3ed2f2d95ee878e
VexTech <herve@intechinfo.fr>
2019-09-09 10:18:12 +0200

    Add controllers

---
+++ ITI.CryptoDatas/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.CryptoDatas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Login()
+        {
+            return "value";
+        }
+
+        [HttpGet]
+        public ActionResult<string> Register()
+        {
+            return "value";
+        }
+
+        [HttpDelete]
+        public ActionResult<string> Delete()
+        {
+            return "value";
+        }
+
+        [HttpPut]
+        public ActionResult<string> Edit()
+        {
+            return "value";
+        }
+
+        // find an implmentation
+        [HttpPost]
+        public ActionResult<string> ToFind()
+        {
+            return "value";
+        }
+    }
+}
---
+++ ITI.CryptoDatas/Controllers/WalletsController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ITI.CryptoDatas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WalletsController : Controller
+    {
+    }
+}
